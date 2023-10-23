06f2c0fbe259ee40b7ec2a06ef48c6c53fc97c94
Raudhil <raudhilfirdausn@gmail.com>
2023-10-23 12:53:47 +0700

    Percobaan 2 : Meminta Inputan Pengguna untuk Mengisi Elemen Array

--- ArrayNilai23.java
+++ ArrayNilai23.java
@@ -9,7 +9,6 @@ public class ArrayNilai23 {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-
         for (int j = 0; j < 10; j++){
             System.out.println("Nilai akhir ke-" + j + " adalah " + nilaiAkhir[j]);
         }
